4592dbcff5d35fe95e39a66ffdff4350e5ed52d1
Michael Peppler <mpeppler@carnegiehall.org>
2016-03-01 18:13:55 -0500

    make the filename unique to the DB

--- model/model.go
+++ model/model.go
@@ -22,7 +22,7 @@ func SetupDb(dbName string) {
 
 	tx := db.MustBegin()
 
-	tx.Exec(`CREATE TABLE IF NOT EXISTS comics (id INTEGER PRIMARY KEY, path TEXT UNIQUE, filename TEXT, series TEXT,
+	tx.Exec(`CREATE TABLE IF NOT EXISTS comics (id INTEGER PRIMARY KEY, path TEXT, filename TEXT UNIQUE, series TEXT,
             size INTEGER, pages INTEGER, issue INTEGER)`)
 	tx.Exec(`CREATE TABLE IF NOT EXISTS dbinfo (name text PRIMARY KEY, value text)`)
 
